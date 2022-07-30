c6f18d30b0530a08eba76b50936ec674489ecb07
AlexAlone <Arhipow.A@inbox.ru>
2022-07-30 15:07:44 +0500

    in progress

--- Test/main.py
+++ Test/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = 'postgresql://db_user:db_password'
 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "It's alive!"
+    test = None
+    test = db.session.execute(text('SELECT "TEST"')).scalar()
+    return f"It's alive! {test}"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
